20964cc0014de3a6ee6709c289a0e147c72b6861
hiroki <hiroki.hkt@gmail.com>
2024-07-18 16:23:02 +0900

    記事を表示するのに、Orvalを使って自動生成された型を使用するように修正

--- src/features/ArticleList.tsx
+++ src/features/ArticleList.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import { QiitaArticle } from '../api/qiita/protoQiitaProto.schemas'
+
+const ArticleListContainer = styled.div`
+  width: 100%;
+`;
 
 const ArticleContainer = styled.div`
   border: 1px solid #ddd;
   padding: 20px;
   margin: 10px 0;
-  border-radius: 5px;
+  border-radius:8px;
   background-color: #f9f9f9;
-  max-width: 36%;
-  margin-left: auto;
-  margin-right: auto;
 `;
 
 const ArticleTitle = styled.h3`
@@ -20,29 +22,22 @@ const ArticleAuthor = styled.p`
   font-style: italic;
 `;
 
-type Article = {
-  title: string;
-  author: string;
-  url: string;
-  published_at: string;
-};
-
 type ArticleListProps = {
-  articles: Article[];
+  articles: QiitaArticle[];
 };
 
 const ArticleList: React.FC<ArticleListProps> = ({ articles }) => {
   return (
-    <div>
+    <ArticleListContainer>
       {articles.map((article, index) => (
         <ArticleContainer key={index}>
           <ArticleTitle>{article.title}</ArticleTitle>
           <ArticleAuthor>by {article.author}</ArticleAuthor>
-          <p>{article.published_at}</p>
+          <p>{article.publishedAt}</p>
           <a href={article.url} target="_blank" rel="noopener noreferrer">記事を読む</a>
         </ArticleContainer>
       ))}
-    </div>
+    </ArticleListContainer>
   );
 };
 
